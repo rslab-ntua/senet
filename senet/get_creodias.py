a0f904564a55eb7d9505b67d235ee7dbff6781da
Claire Richert <richert.claire@gmail.com>
2024-06-11 11:32:50 +0200

    modify request to get data information from copernicus dataspace

--- senet/get_creodias.py
+++ senet/get_creodias.py
@@ -1,8 +1,10 @@
 import os
-import pandas as pd
-from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
-from creodias_finder import query
 from datetime import datetime
+
+import pandas as pd
+import requests
+from creodias_finder import query
+from sentinelsat import read_geojson, geojson_to_wkt
 
 EO_PRODUCT_TYPES = {
     "Sentinel-1": {
@@ -44,7 +46,10 @@ EO_INSTRUMENTS = {
     }
 }
 
-def eodata_path_creator(data:pd.DataFrame):
+CDS_URL = "https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=Collection/Name"
+
+
+def eodata_path_creator(data: pd.DataFrame):
     """Convert a DataFrame with the response from APIHUB to CreoDIAS paths. Works with Sentinel-1, 2, 3 (all instruments).
     Function builds paths as follows:
     S1: /eodata/platform/instrumentshortname/producttype/year/month/day/filename
@@ -128,26 +133,39 @@ def get_data_DIAS(area:str, start_date:str, end_date:str, platform:str = "Sentin
 
     return data
 
-def get_data(area:str, start_date:str, end_date:str, username:str, password:str, platform:str = "Sentinel-2", **kwargs):
-    """Get data information from ESA APIHUB.
+
+def get_data(area: str, start_date: str, end_date: str, platform: str, product_type: str, max_cloud_cover=100):
+    """Get data information from Copernicus dataspace.
 
     Args:
         area (str): Path to geometry file (geojson)
-        start_date (str): Start date in YYYYMMDD format
-        end_date (str): End date in YYYYMMDD format
-        username (str): APIHUB username
-        password (str): APIHUB password
-        platform (str, optional): Platform name. Defaults to "Sentinel-2"
-        **kwargs: Check https://scihub.copernicus.eu/twiki/do/view/SciHubUserGuide/FullTextSearch?redirectedfrom=SciHubUserGuide.3FullTextSearch
-
+        start_date (str): Start date in YYYY-MM-DD format
+        end_date (str): End date in YYYY-MM-DD format
+        platform (str): Platform name. Example: "SENTINEL-2"
+        product_type (str): product name. Example "S2MSI2A"
+        max_cloud_cover (int, optional): maximum cloud cover in percentage to filter images. Default to 100.
     Returns:
         DataFrame: APIHUB response with the available data converted to DataFrame
     """
-    api = SentinelAPI(f"{username}", f"{password}", "https://apihub.copernicus.eu/apihub/")
     footprint = geojson_to_wkt(read_geojson(area))
-    products = api.query(footprint, date = (start_date, end_date), platformname = platform, **kwargs)
-    products_df = api.to_dataframe(products)
+    query_products = (f"{CDS_URL} eq '{platform}' and OData.CSC.Intersects(area=geography'SRID=4326;{footprint}') and"
+                      f" ContentDate/Start gt {start_date}T00:00:00.000Z and "
+                      f"ContentDate/Start lt {end_date}T00:00:00.000Z and "
+                      "Attributes/OData.CSC.StringAttribute/any(att:att/Name eq 'productType' and "
+                      f"att/OData/CSC.StringAttribute/Value eq '{product_type}')")
+    # For Sentinel-3 data: when max cloud cover is given in the request, the query always returns an empty json
+    if max_cloud_cover < 100:
+        query_products += (f" and Attributes/OData.CSC.DoubleAttribute/any(att:att/Name eq 'cloudCover' and "
+                           f"att/OData.CSC.DoubleAttribute/Value le {max_cloud_cover})")
+
+    query_products += "&$top=1000"
+
+    products = requests.get(query_products).json()
+    if 'value' not in products.keys():
+        raise ValueError("Bad request")
+    products_df = pd.DataFrame.from_dict(products['value'])
     if products_df.empty:
-        raise ValueError("ESA SciHUB returned empty request! This is either a bad request or there are no data available in the selected date range!")
-    
+        raise ValueError("Copernicus dataspace returned empty request! There are no "
+                         "data available in the selected date range!")
+
     return products_df
